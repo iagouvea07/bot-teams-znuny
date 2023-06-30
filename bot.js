fed1b3f88e895d0483834b06cc763a1bdc8897c2
iagouvea07 <igouvea@outlook.com>
2023-06-29 23:13:35 -0300

    update

--- bot.js
+++ bot.js
@@ -4,35 +4,40 @@ const axios = require('axios')
 class EchoBot extends ActivityHandler {
     constructor() {
         super()
-
         this.onMessage(async (context, next) => {
             if(context.activity.text.startsWith("!session ")){
                 const content = context.activity.text.replace('!session ', '')
                 var replyText = content;
-
-                const url = "http://127.0.0.1/otrs/nph-genericinterface.pl/Webservice/api-v2/session"
-
-                const params = {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        UserLogin: "root@localhost",
-                        Password: "102030"
-                      })
-                }
-
-                const response = await axios.post(url, params.body)
-                const data = response.data
                 
-                console.log(data)
-                replyText = content.split(' ').map(() => data['SessionID']).join(' ')
+                switch(replyText){
+                    case "create":
+                        const url = `http://${process.env.FQDN}/otrs/nph-genericinterface.pl/Webservice/api-v2/session`
+                        const username = process.env.USER
+                        const password = process.env.PASSWORD
+                        
+                        const params = {
+                            method: "POST",
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                UserLogin: username,
+                                Password: password
+                            })
+                        }
 
-                await context.sendActivity(MessageFactory.text(replyText, replyText));
-            }
-            else{
-                const replyText = `Echo: ${ context.activity.text }`;
+                        const response = await axios.post(url, params.body)
+                        const data = response.data
+                        
+                        console.log(data)
+                        replyText = content.split(' ').map(() => data['SessionID']).join(' ')
+                        break;
+
+                    default:
+                        replyText = `I Don't understand, patron! Please try tell me again...`;
+                        break;
+                }
+                     
                 await context.sendActivity(MessageFactory.text(replyText, replyText));
             }
             
@@ -41,12 +46,14 @@ class EchoBot extends ActivityHandler {
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Welcome! My name is Botinho. Tell me your command, Patron!';
+
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
                 }
             }
+
             await next();
         });
     }
